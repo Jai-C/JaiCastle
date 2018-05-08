58df636bba4b8efec62f84527be6d3d9b179cc17
Jai-C <jaicastle0@gmail.com>
2018-05-08 20:44:16 +0800

    Create main.java

---
+++ UWA Project 1/FifteenPuzzle/main.java
@@ -0,0 +1,12 @@
+public class main {
+    
+    public static void main(String[] args){
+        FifteenPuzzle puzzle = new FifteenPuzzle();
+        
+        /* Option to start the game with a set grid:
+        int[][] example = {{5,11,14,0}, {9,3,13, 7}, {2,8,10,12}, {4,1,15, 6}};
+        FifteenPuzzle puzzle2 = new FifteenPuzzle(example);
+        */
+        
+    }
+}
